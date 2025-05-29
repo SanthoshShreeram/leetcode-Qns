63b1531b3d6ccca2c08d4aeab3ecbe83b792f040
Santhosh Shreeram <shreeramsanthosh1@gmail.com>
2025-05-29 16:39:03 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.5 MB (5.82%)

---
+++ 0035-search-insert-position/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(target==nums[i] || target<nums[i]){
+                return i;
+            }
+            else if(target>nums[i])
+            {
+                j++;
+            }
+        }
+        return j;
+    }
+}
