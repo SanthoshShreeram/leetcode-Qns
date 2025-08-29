0ba6390f99b7a5d684bc1b1d0bb2b1c7e63077f2
Santhosh Shreeram <shreeramsanthosh1@gmail.com>
2025-08-29 11:17:47 +0000

    [LeetCode Sync] Runtime - 1 ms (84.29%), Memory - 41.2 MB (30.11%)

--- 0069-sqrtx/solution.java
+++ 0069-sqrtx/solution.java
@@ -1,16 +1,18 @@
 class Solution {
     public int mySqrt(int x) {
-	if (x == 0) return 0;
-	int start = 1, end = x;
-	while (start < end) { 
-		int mid = start + (end - start) / 2;
-		if (mid <= x / mid && (mid + 1) > x / (mid + 1))
-			return mid; 
-		else if (mid > x / mid)
-			end = mid;
-		else
-			start = mid + 1;
-	}
-	return start;
-}
+        int start = 0, end = x, mid = 0;
+        while (start<=end) {
+            mid = start + (end - start)/2;
+            if((long)mid*mid<=x && (long)(mid + 1)*(mid + 1) > x ) {
+                return mid;
+            }
+            else if ((long)mid*mid < x) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
 }
