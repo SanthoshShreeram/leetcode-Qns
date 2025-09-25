238cafd388fea77919536b523cffa9b69b4dfd4c
Santhosh Shreeram <shreeramsanthosh1@gmail.com>
2025-09-25 17:47:28 +0000

    [LeetCode Sync] Runtime - 3 ms (82.55%), Memory - 45.4 MB (76.98%)

--- 0120-triangle/solution.java
+++ 0120-triangle/solution.java
@@ -1,11 +1,18 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int[] A = new int[triangle.size()+1];
-        for(int i=triangle.size()-1;i>=0;i--){
-            for(int j=0;j<triangle.get(i).size();j++){
-                A[j] = Math.min(A[j],A[j+1])+triangle.get(i).get(j);
+        int row = triangle.size();
+        int[] memo = new int[row];
+
+        for (int i = 0; i < row; i++) {
+            memo[i] = triangle.get(row - 1).get(i);
+        }
+
+        for (int r = row-2; r >= 0; r--) {
+            for (int c = 0; c <= r; c++) {
+                memo[c] = Math.min(memo[c], memo[c+1]) + triangle.get(r).get(c);
             }
         }
-        return A[0];
+
+        return memo[0];        
     }
 }
