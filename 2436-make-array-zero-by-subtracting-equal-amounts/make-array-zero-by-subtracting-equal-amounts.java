8c7fd975d6ff752fc0f9a523a24eff5f4102c27b
Santhosh <124123012+SanthoshShreeram@users.noreply.github.com>
2025-11-07 19:49:03 +0530

    Time: 2 ms (21.56%) | Memory: 43 MB (6.05%) - LeetSync

---
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int operations = 0;
+        while(true){
+            int min = Integer.MAX_VALUE;
+            for (int num: nums){
+                if(num > 0 && num < min){
+                    min = num;
+                }
+            }
+            if(min == Integer.MAX_VALUE) break;
+            for(int i = 0; i < nums.length; i++){
+                if(nums[i] > 0){
+                    nums[i] -= min;
+                }
+            }
+            operations++;
+        }
+        return operations;
+    }
+}
